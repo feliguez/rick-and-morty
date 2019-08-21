a66f1b69df35099b31cb2b95bb99b99b7d9f74fc
Felipe Rodríguez <feliperodriguez@MacBook-Pro-de-Felipe.local>
2019-08-21 17:44:10 -0400

    Mount Heroes view

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./bootstrap.css";
 import "./App.css";
 import Home from "./views/home/Home";
+import Heroes from "./views/heroes/Heroes";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <Heroes />
     </div>
   );
 }
