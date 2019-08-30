a2b80b1213fff11858cc22eb52eb22fd38d0a055
Felipe Rodríguez <felipe.rodriguezn@gmail.com>
2019-08-30 19:02:00 -0400

    Add isKill and useRing properties

--- src/services/hero.services.js
+++ src/services/hero.services.js
@@ -1,7 +1,42 @@
 export const heroes = [
-  { name: "Gandalf", race: "Maia", age: "2019", weapon: "Staff 🏑" },
-  { name: "Aragorn", race: "Human", age: "120", weapon: "Sword ⚔" },
-  { name: "Legolas", race: "Elf", age: "200", weapon: "Bow 🏹" },
-  { name: "Gimli", race: "Dwarf", age: "139", weapon: "Axe ⚒" },
-  { name: "Frodo", race: "Hobbit", age: "33", weapon: "Dagger 🗡" }
+  {
+    name: 'Gandalf',
+    race: 'Maia',
+    age: '2019',
+    weapon: 'Staff 🏑',
+    isKill: false,
+    useRing: false
+  },
+  {
+    name: 'Aragorn',
+    race: 'Human',
+    age: '120',
+    weapon: 'Sword ⚔',
+    isKill: false,
+    useRing: false
+  },
+  {
+    name: 'Legolas',
+    race: 'Elf',
+    age: '200',
+    weapon: 'Bow 🏹',
+    isKill: false,
+    useRing: false
+  },
+  {
+    name: 'Gimli',
+    race: 'Dwarf',
+    age: '139',
+    weapon: 'Axe ⚒',
+    isKill: false,
+    useRing: false
+  },
+  {
+    name: 'Frodo',
+    race: 'Hobbit',
+    age: '33',
+    weapon: 'Dagger 🗡',
+    isKill: false,
+    useRing: false
+  }
 ];
