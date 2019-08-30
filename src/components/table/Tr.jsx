b319bcc7464e595038f8c30cce4c443553d3beaf
Felipe Rodríguez <felipe.rodriguezn@gmail.com>
2019-08-30 18:52:54 -0400

    Set isKill and useRing styles

--- src/components/table/Tr.jsx
+++ src/components/table/Tr.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Tr = props => {
-  return <tr className="heroes-row">{props.children}</tr>;
+  return (
+    <tr
+      className={`heroes-row ${props.isKill ? 'isKill' : ''} ${
+        props.useRing ? 'useRing' : ''
+      }`}
+    >
+      {props.children}
+    </tr>
+  );
 };
 
 export default Tr;
