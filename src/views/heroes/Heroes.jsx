44f9e97dc20236d34486e9c1a20055208842b204
Felipe Rodríguez <felipe.rodriguezn@gmail.com>
2019-10-03 15:52:13 -0300

    Clean Heroe view

--- src/views/heroes/Heroes.jsx
+++ src/views/heroes/Heroes.jsx
@@ -1,70 +1,18 @@
 import React from 'react';
-import { connect, batch } from 'react-redux';
 import Form from '../../components/form/Form';
 import HeroesTable from '../../components/heroesTable/HeroesTable';
-
-import {
-  ringHeroe,
-  removeHeroe,
-  addHeroe,
-  editHeroe,
-  updateHeroe,
-  filterHeroe
-} from '../../store/actions/heroe.actions';
 
 import './Heroes.css';
 import Search from '../../components/search/Search';
 
-const Heroes = props => {
-  const {
-    addHeroe,
-    counter,
-    editHeroe,
-    filterHeroe,
-    heroes,
-    isRingUsed,
-    removeHeroe,
-    ringHeroe,
-    updateHeroe
-  } = props;
-
+const Heroes = () => {
   return (
     <>
-      <Form addHeroe={addHeroe} counter={counter} />
-      <Search filterHeroe={filterHeroe} />
-      <HeroesTable
-        editHeroe={editHeroe}
-        heroes={heroes.filter(hero => hero.show)}
-        isRingUsed={isRingUsed}
-        removeHeroe={removeHeroe}
-        ringHeroe={ringHeroe}
-        updateHeroe={updateHeroe}
-      />
+      <Form />
+      <Search />
+      <HeroesTable />
     </>
   );
 };
 
-const mapStatetoProps = state => state.heroes;
-
-const mapDispatchToProps = dispatch => {
-  return {
-    addHeroe: heroes => {
-      batch(() => {
-        dispatch(addHeroe(heroes));
-        // dispatch(increaseCounter());
-      });
-    },
-    ringHeroe: heroes => {
-      dispatch(ringHeroe(heroes));
-    },
-    removeHeroe: heroes => dispatch(removeHeroe(heroes)),
-    editHeroe: heroes => dispatch(editHeroe(heroes)),
-    updateHeroe: heroes => dispatch(updateHeroe(heroes)),
-    filterHeroe: heroes => dispatch(filterHeroe(heroes))
-  };
-};
-
-export default connect(
-  mapStatetoProps,
-  mapDispatchToProps
-)(Heroes);
+export default Heroes;
