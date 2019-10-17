0e61145201ee7c7a9b2eac9f590e5d332f49711c
Felipe Rodríguez <felipe.rodriguezn@gmail.com>
2019-10-17 13:49:55 -0300

    Updates

---
+++ precache-manifest.6c282d9d2b735092a6f620bcfa3036f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64bb882b9f1f5040ca0f13762fb53f67",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "dcd37deb2348034d5153",
+    "url": "/rick-and-morty/static/css/main.fcd278e2.chunk.css"
+  },
+  {
+    "revision": "edb5a99b8038fc334faf",
+    "url": "/rick-and-morty/static/js/2.3adaef0f.chunk.js"
+  },
+  {
+    "revision": "dcd37deb2348034d5153",
+    "url": "/rick-and-morty/static/js/main.8b447c87.chunk.js"
+  },
+  {
+    "revision": "c2f344254506590b47d9",
+    "url": "/rick-and-morty/static/js/runtime~main.8a2ddac7.js"
+  }
+]);
